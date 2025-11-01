4fbd0a8f186b49fae5ddb20a8c394c5991b09cc8
Shubh Barnwal <barnwalshubh8002434@gmail.com>
2025-11-01 12:25:23 +0530

    Changes in frontend folder

--- portfolio-frontend/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Removed swcMinify as it's not supported in your Next.js version
-  // Add any other Next.js config options here
-}
-
-module.exports = nextConfig
